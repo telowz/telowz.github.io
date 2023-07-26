6ac64823a508fcc75374520a8a73532e5c8ca2f2
telowz <129795135+telowz@users.noreply.github.com>
2023-07-26 22:28:39 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function handleButtonClick() {
+  let alertText = window.alert('Stalk my github (telowz)');
+}
+
+const telowzButton = document.querySelector('.telowz');
+telowzButton.addEventListener('click', handleButtonClick);
 
