37ef720543a7a6fc83b37902ea704381151697f9
telowz <129795135+telowz@users.noreply.github.com>
2023-08-29 17:31:33 +1200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function handleButtonClick() {
-  let alertText = window.alert('Stalk my github (telowz)');
-}
-
-const telowzButton = document.querySelector('.telowz');
-telowzButton.addEventListener('click', handleButtonClick);
-
